2d552f005c7d3cc647bee5c98d0f6cc1962b3c13
Davidpereira2803 <david.pereira.002@student.uni.lu>
2025-07-17 23:14:10 +0200

    Refactor: Remove Android native files and configurations
    - Deleted MainActivity.kt and MainApplication.kt to streamline the Android native code.
    - Removed splash screen images and related drawable resources.
    - Cleared out unused XML files for launcher icons and styles.
    - Eliminated gradle files and configurations to simplify the build process.
    - Updated app.json to include Firebase configuration directly.
    - Adjusted eas.json to source app version from remote.
    - Refactored firebase.js to directly use constants from expoConfig for Firebase setup.

--- services/firebase.js
+++ services/firebase.js
@@ -5,27 +5,26 @@ import { getStorage } from "firebase/storage";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Constants from 'expo-constants';
 
-import { 
-  FIREBASE_API_KEY as ENV_API_KEY,
-  FIREBASE_AUTH_DOMAIN as ENV_AUTH_DOMAIN,
-  FIREBASE_PROJECT_ID as ENV_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET as ENV_STORAGE_BUCKET,
-  FIREBASE_MESSAGING_SENDER_ID as ENV_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID as ENV_APP_ID,
-  FIREBASE_MEASUREMENT_ID as ENV_MEASUREMENT_ID
-} from "@env";
-
-const extra = Constants.expoConfig?.extra ?? {};
+const {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID
+} = Constants.expoConfig.extra;
 
 const firebaseConfig = {
-  apiKey: extra.FIREBASE_API_KEY || ENV_API_KEY,
-  authDomain: extra.FIREBASE_AUTH_DOMAIN || ENV_AUTH_DOMAIN,
-  projectId: extra.FIREBASE_PROJECT_ID || ENV_PROJECT_ID,
-  storageBucket: extra.FIREBASE_STORAGE_BUCKET || ENV_STORAGE_BUCKET,
-  messagingSenderId: extra.FIREBASE_MESSAGING_SENDER_ID || ENV_MESSAGING_SENDER_ID,
-  appId: extra.FIREBASE_APP_ID || ENV_APP_ID,
-  measurementId: extra.FIREBASE_MEASUREMENT_ID || ENV_MEASUREMENT_ID
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
