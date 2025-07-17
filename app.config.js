d41c4043b502219bee7963b804caf0a1424c88a8
Davidpereira2803 <david.pereira.002@student.uni.lu>
2025-07-17 21:22:30 +0200

    feat: Update app configuration and integrate Firebase settings

--- app.config.js
+++ app.config.js
@@ -2,33 +2,44 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: "LetzGrade",
-    slug: "letzgrade",
+    name: "letzgrade-mobile",
+    slug: "letzgrade-mobile",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
     splash: {
-      image: "./assets/splash.png",
+      image: "./assets/splash-icon.png",
       resizeMode: "contain",
       backgroundColor: "#ffffff"
     },
-    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true
+    },
     android: {
-      package: "com.yourname.letzgrade",
-      versionCode: 1,
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#FFFFFF"
-      }
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      package: "com.davidpereira2803.letzgrademobile"
+    },
+    web: {
+      favicon: "./assets/favicon.png"
     },
     extra: {
-      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-      FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-      FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+      eas: {
+        projectId: "d75b128e-0a54-45dd-9f58-677bca005d53"
+      },
+      firebase: {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID
+      }
     }
   }
-};
+}
