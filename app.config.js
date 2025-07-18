e891b6aa32e82f5513cebf4950630989b252f5f6
Davidpereira2803 <david.pereira.002@student.uni.lu>
2025-07-18 23:10:02 +0200

    fix: Update app configuration structure to ensure proper expo integration

--- app.config.js
+++ app.config.js
@@ -1,44 +1,46 @@
 import 'dotenv/config';
 
 export default ({ config }) => ({
-  ...config,
-  name: "LetzGrade",
-  slug: "letzgrade-mobile",
-  version: "1.0.0",
-  orientation: "portrait",
-  icon: "./assets/icon.png",
-  userInterfaceStyle: "light",
-  newArchEnabled: true,
-  splash: {
-    image: "./assets/splash-icon.png",
-    resizeMode: "contain",
-    backgroundColor: "#ffffff"
-  },
-  ios: {
-    supportsTablet: true
-  },
-  android: {
-    permissions: ["INTERNET"],
-    adaptiveIcon: {
-      foregroundImage: "./assets/adaptive-icon.png",
-      backgroundColor: "#ffffff"
+  expo: {
+    ...config.expo,
+    name: "LetzGrade",
+    slug: "letzgrade-mobile",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
-    edgeToEdgeEnabled: true,
-    package: "com.davidpereira2803.letzgrademobile"
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      permissions: ["INTERNET"],
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
+      package: "com.davidpereira2803.letzgrademobile",
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+      FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+      FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+      eas: {
+        projectId: "d75b128e-0a54-45dd-9f58-677bca005d53",
+      },
+    },
   },
-  web: {
-    favicon: "./assets/favicon.png"
-  },
-  extra: {
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
-    eas: {
-      projectId: "d75b128e-0a54-45dd-9f58-677bca005d53"
-    }
-  }
 });
